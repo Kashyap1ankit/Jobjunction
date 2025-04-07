dea27b3a24610a5847a2ca262f875b9c4ba0cb36
Ankit <kashyap25ankit@gmail.com>
2025-04-08 02:22:02 +0530

    added seo optimisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,42 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Job Junction",
-  description: "Help Others",
+  description:
+    "Find your Next Opportunity With Job Junction. Whether you're looking for your next career move or you know a great opportunity for others.",
   icons: "Images/jj-logo.png",
+  keywords:
+    "job junction, job portals, job websites, freshers jobs, job vacancies, career opportunities, india job search, job portal india",
+
+  robots: "index, follow",
+  openGraph: {
+    title: "Job Junction",
+    description:
+      "Find your Next Opportunity With Job Junction. Whether you're looking for your next career move or you know a great opportunity for others.",
+    url: "https://jj.10xdevlab.in/",
+    images: [
+      {
+        url: "https://jj.10xdevlab.in/Images/jj-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Job Junction Logo",
+      },
+    ],
+    siteName: "Job Junction",
+    type: "website",
+    locale: "en_IN",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Job Junction",
+    description:
+      "Find your Next Opportunity With Job Junction. Whether you're looking for your next career move or you know a great opportunity for others.",
+    creator: "@kashyap_tweetts",
+    images: ["https://jj.10xdevlab.in/Images/jj-logo.png"],
+  },
+  alternates: {
+    canonical: "https://jj.10xdevlab.in",
+  },
 };
 
 export default async function RootLayout({
